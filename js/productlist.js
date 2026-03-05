e790a5f39c9977241fd9d34f55ac18bc84c7aa20
William <wn300706@gmail.com>
2026-03-05 23:14:19 +0100

    sortering

--- js/productlist.js
+++ js/productlist.js
@@ -6,33 +6,66 @@ const klikkategori = new URLSearchParams(window.location.search).get(
 
 console.log(klikkategori);
 
+document.querySelector("h2").textContent = klikkategori;
+
 const container = document.querySelector(".grid-products");
 
 const endpoint = `https://kea-alt-del.dk/t7/api/products?category=${klikkategori}&limit=30`;
 
 document
-  .querySelectorAll("button")
+  .querySelectorAll("#filter button")
   .forEach((knap) => knap.addEventListener("click", filter));
 
+document
+  .querySelectorAll("#sorter button")
+  .forEach((knap) => knap.addEventListener("click", sorter));
+
 let allData;
+let udsnit;
 
 function getData() {
   fetch(endpoint)
     .then((res) => res.json())
     .then((data) => {
       allData = data;
+      udsnit = data;
       showProducts(allData);
     });
 }
 
 function filter(e) {
   const valgt = e.target.textContent;
-  if (valgt == "All") {
-    showProducts(allData);
+  console.log(valgt);
+  if (valgt === "All") {
+    udsnit = allData;
+    showProducts(udsnit);
   } else {
-    const udsnit = allData.filter((element) => element.gender == valgt);
+    udsnit = allData.filter((element) => element.gender === valgt);
     showProducts(udsnit);
   }
+}
+
+function sorter(e) {
+  if (e.target.dataset.price) {
+    const dir = e.target.dataset.price;
+    if (dir === "up") {
+      udsnit.sort((a, b) => a.price - b.price);
+    } else {
+      udsnit.sort((a, b) => b.price - a.price);
+    }
+  } else {
+    const dir = e.target.dataset.text;
+    if (dir === "az") {
+      udsnit.sort((a, b) =>
+        a.productdisplayname.localeCompare(b.productdisplayname, "da"),
+      );
+    } else {
+      udsnit.sort((a, b) =>
+        b.productdisplayname.localeCompare(a.productdisplayname, "da"),
+      );
+    }
+  }
+  showProducts(udsnit);
 }
 
 function showProducts(produkter) {
@@ -47,7 +80,7 @@ function showProducts(produkter) {
             ${produkt.soldout ? '<span class="soldout">Sold Out</span>' : ""}
             <img src="https://kea-alt-del.dk/t7/images/webp/640/${produkt.id}.webp" />
             <h3>${produkt.productdisplayname}</h3>
-            <p class="brand">T-shirts | Puma</p>
+            <p class="brand">${produkt.articletype} | ${produkt.brandname}</p>
             <p class="old-price">${produkt.price},-</p>
             <div class="discount">
             <p class="price-square">NOW DKK <span>${Math.round(produkt.price - (produkt.price * produkt.discount) / 100)}</span>,-</p>
